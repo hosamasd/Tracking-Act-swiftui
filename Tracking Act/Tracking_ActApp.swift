a85b3fbddb22eb0b553d9c917d85dc3ab0b7ef53
hosam <hosamelmalt@gmail.com>
2020-11-17 08:26:08 +0200

    Initial Commit

---
+++ Tracking Act/Tracking_ActApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tracking_ActApp.swift
+//  Tracking Act
+//
+//  Created by hosam on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Tracking_ActApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
